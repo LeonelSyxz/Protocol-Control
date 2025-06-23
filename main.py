d15146f523cc3b60f84058bbcb0bf9a34fa07c1c
Edgar Leonel Acevedo Cuevas <leonelsyxz@gmail.com>
2025-06-23 15:29:26 -0600

    Refactor main.py and ui/main_window.py to support resource paths for bundled assets; update main.spec to include icono.ico; modify xref-main.html for import documentation; remove control.exe; update warn-main.txt for missing modules; regenerate binary files.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# main.py
-
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
 from ui.main_window import MainWindow
 from database.db import init_db
 import sys
+import os
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.abspath(relative_path)
 
 if __name__ == "__main__":
-    init_db() 
+    init_db()
 
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("icono.ico"))
+    app.setWindowIcon(QIcon(resource_path("icono.ico")))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
--- ui/main_window.py
+++ ui/main_window.py
@@ -4,11 +4,19 @@ from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QTabWidget, QLa
 from PySide6.QtGui import QIcon
 from PySide6.QtCore import Qt
 from ui.tabs.hls import HLSTab
+import sys, os
+
+def resource_path(relative_path):
+    """ Devuelve la ruta absoluta del archivo, compatible con PyInstaller """
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+        return os.path.abspath(relative_path)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Protocol Control")
+        self.setWindowIcon(QIcon(resource_path("icono.ico")))
         self.setMinimumSize(800, 600)
 
         self.tabs = QTabWidget()
