88c785d521be595f8c230db86955992f91ae7e15
Edgar Leonel Acevedo Cuevas <leonelsyxz@gmail.com>
2025-06-23 15:17:46 -0600

    Set application window icon in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from ui.main_window import MainWindow
 from database.db import init_db
 import sys
@@ -9,6 +10,7 @@ if __name__ == "__main__":
     init_db() 
 
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icono.ico"))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
